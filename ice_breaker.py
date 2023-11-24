65256b763e221f5b2ae2bd49e7c1b6c6ce836bc0
rasicaknambiar <rasicaknambiar@gmail.com>
2023-11-24 11:15:55 +0530

    Added linkedin processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,15 +4,15 @@ from langchain.chains import LLMChain
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    print("Hello LangChain")
 
-    linkedin_profile_url = linkedin_lookup_agent(name="Harrison Chase")
-
+def ice_break(name: str) -> str:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
     summary_template = """
         Given the Linkedin information {information} about a person from which I want you to create:
         1. a short summary
         2. two interesting facts about them
+        3. two creative ice breakers to open a conversation with them
         """
 
     summary_prompt_template = PromptTemplate(
@@ -21,6 +21,11 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    result = chain.run(information=linkedin_data)
+    print(result)
+    return result
 
-    print(chain.run(information=linkedin_data))
+
+if __name__ == "__main__":
+    print("Hello LangChain")
+    result = ice_break(name="Harrison Chase")

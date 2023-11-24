216f2f0465de692b65e8a64908270573007996aa
rasicaknambiar <rasicaknambiar@gmail.com>
2023-11-24 14:48:17 +0530

    Added linkedin processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,8 @@
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+
+from output_parsers import person_intel_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
@@ -13,10 +15,15 @@ def ice_break(name: str) -> str:
         1. a short summary
         2. two interesting facts about them
         3. two creative ice breakers to open a conversation with them
+        \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
@@ -28,4 +35,4 @@ def ice_break(name: str) -> str:
 
 if __name__ == "__main__":
     print("Hello LangChain")
-    result = ice_break(name="Harrison Chase")
+    result = ice_break(name="Carl Pei")
